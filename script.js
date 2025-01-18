8d34995011d5e77954695576d56e51d80e31e536
chandan vishwakarma <chandanvishw11002@gmail.com>
2025-01-18 22:42:33 +0530

    btn modified

--- script.js
+++ script.js
@@ -1,43 +1,46 @@
-document.getElementById("btn").addEventListener("click", function(){
-    const input = document.getElementById('inputNumber').value;
+document.getElementById("btn").addEventListener("click", function () {
+  const input = document.getElementById("inputNumber").value;
 
-   
+  const output = document.getElementById("output");
 
-    const output = document.getElementById("output");
-    if (!/^\d+$/.test(input.trim())) {
-        output.textContent = "Please enter a valid positive number (no letters or symbols).";
-        return;
-      }
-    const number =parseInt(input,10);
 
-    if(isNaN(number) || input.trim()===""){
-        output.textContent = "please enter a number";
-        return
+
+  if (!/^\d+$/.test(input.trim())) {
+    output.textContent =
+      "Please enter a valid positive number (no letters,  symbols, or might be empty!).";
+    return;
+  }
+
+  const number = parseInt(input, 10);
+
+  if (isNaN(number) || input.trim() === "") {
+    output.textContent = "please enter a number";
+    return;
+  }
+
+  if (isNaN(number)) {
+    output.textContent = "please enter a number";
+    return;
+  }
+
+  if (number < 0) {
+    output.textContent = "Enter a positive value.";
+    return;
+  }
+
+  let result = [];
+
+  if (number % 2 === 0) {
+    for (let i = 1; i <= 3; i++) {
+      result.push(number + i * 2);
     }
-
-    if(isNaN(number)){
-        output.textContent = "please enter a number";
-        return
+  } else {
+    for (let i = 1; i <= 3; i++) {
+      result.push(number + 1 * 2);
     }
+  }
 
-
-    if(number < 0){
-        output.textContent = "Enter a positive value."
-        return;
-    }
-
-    let result = [];
-
-    if(number % 2 === 0){
-        for(let i=1; i<=3; i++){
-            result.push(number+i*2);
-        }
-    }else{
-        for(let i = 1; i<=3; i++){
-            result.push(number + 1 *2)
-        }
-    }
-
-    output.textContent = `Next 3 ${number % 2 === 0 ? "even" : "odd"} number : ${result.join(", ")} `
-
-})
+  output.textContent = `Next 3 ${
+    number % 2 === 0 ? "even" : "odd"
+  } number : ${result.join(", ")} `;
+});
